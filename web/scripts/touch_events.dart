c34657d63609fcc0b38f2ba0f6aa3486969b137a
Rasarts <rasart.pro@gmail.com>
2015-08-10 15:30:58 +0300

    update touch events

--- web/scripts/touch_events.dart
+++ web/scripts/touch_events.dart
@@ -1,27 +1,38 @@
 part of w3guru;
 
 makeTouchEvents() {
-  var xStart;
-  var xUpdate;
+  var xStart,
+      xUpdate;
+  var topScrollPoint,
+      updatedTopScrollPoint;
 
   window.onTouchStart.listen((event) {
+    topScrollPoint = window.scrollY;
     xStart = event.touches[0].client.x;
   });
 
   window.onTouchMove.listen((event) {
+    updatedTopScrollPoint = window.scrollY;
     xUpdate = event.touches[0].client.x;
   });
 
   window.onTouchEnd.listen((event) {
 
-    if (xUpdate + 150 > xStart) {
-      /* left swipe */
-      activatePrevPost();
-    } else {
-      /* right swipe */
-      activateNextPost();
+    var toDownScroll = topScrollPoint > updatedTopScrollPoint;
+    var toTopScroll = topScrollPoint < updatedTopScrollPoint;
+
+    if(!toDownScroll && !toTopScroll ){
+      if (xUpdate > xStart) {
+        /* left swipe */
+        activatePrevPost();
+      } else {
+        /* right swipe */
+        activateNextPost();
+      }
     }
 
-    xStart = null;
+    topScrollPoint = 0;
+    xStart = 0;
+    xUpdate = 0;
   });
 }
