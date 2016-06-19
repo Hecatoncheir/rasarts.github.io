f9184dbaa37e00b98a5961915ee3074662b48b54
Vitaliy Vostrikov <rasart.pro@gmail.com>
2016-06-19 23:35:44 +0300

    update router

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -9,6 +9,8 @@ import 'package:route/url_pattern.dart';
 
 import 'package:blog/components/ink_transition/ink_transition.dart';
 
+import 'package:polymer/polymer.dart';
+
 final homeUrl = new UrlPattern(r'/');
 final mainPage = new UrlPattern(r'/#');
 final articleUrl = new UrlPattern(r'/#article/(\w+)');
@@ -19,6 +21,11 @@ Event loadingPageEvent;
 Event readyPageEvent;
 
 InkTransition articleFullContentBlock;
+List articles;
+
+class NullTreeSanitizer implements NodeTreeSanitizer {
+  void sanitizeTree(node) {}
+}
 
 prepareRouter() async {
   loadingPageEvent = new CustomEvent('Page loading');
@@ -28,8 +35,8 @@ prepareRouter() async {
 
   router = new Router(useFragment: true);
 
-  document.on['Main page must be open'].listen((event){
-    router.gotoPath('/#', 'Main page');
+  document.on['Main page must be open'].listen((event) {
+    router.gotoPath('/#', 'Vitaliy Vostrikov Blog');
   });
 
   router
@@ -45,6 +52,43 @@ showHome(String path) async {
   articleFullContentBlock.close();
   articleFullContentBlock.set('header', null);
   articleFullContentBlock.set('fullDetails', '');
+
+  Element pageHome = querySelector('#page-home');
+
+  if (articles == null || articles.isEmpty) {
+    String articlesJSON =
+        await HttpRequest.getString('/articles/articles.json');
+    articles = JSON.decode(articlesJSON);
+
+    for (int articleId = 0; articleId < articles.length; articleId++) {
+      String articleDetailsJSON =
+          await HttpRequest.getString('/articles/${articles[articleId]}.json');
+      Map articleDetails = JSON.decode(articleDetailsJSON);
+      print(articleDetails);
+
+      if (articleId < 5){
+        pageHome.appendHtml('''
+         <header class="bp-header cf style-scope stack-pages">
+
+            <a href="/#article/${articles[articleId]}">
+                <ink-button class="ink-btn style-scope stack-pages">Открыть</ink-button>
+            </a>
+
+            <span class="bp-header__present style-scope stack-pages">Blueprint</span>
+            <a class="style-scope stack-pages" href="/#article/${articles[articleId]}">
+              <h1 class="bp-header__title style-scope stack-pages">${articleDetails['title']}</h1>
+            </a>
+            <p class="bp-header__desc style-scope stack-pages">Based on Ilya Kostin's Dribbble shot
+                <a class="style-scope stack-pages"
+                   href="https://dribbble.com/shots/2286042-Stacked-navigation">Stacked navigation</a>
+            </p>
+
+        </header>
+        ''', treeSanitizer: new NullTreeSanitizer());
+      }
+
+    }
+  }
 
   document.dispatchEvent(readyPageEvent);
 }
